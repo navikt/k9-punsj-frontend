d749f7348422908059599df86692efda4247c81c
Engebretsen <tomas.engebretsen@nav.no>
2019-11-14 15:35:29 +0100

    Legger til server.js

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const mustacheExpress = require('mustache-express');
+const Promise = require('promise');
+const compression = require('compression');
+const helmet = require('helmet');
+const createEnvSettingsFile = require('./src/build/scripts/envSettings');
+
+const server = express();
+server.use(helmet());
+server.use(compression());
+server.set('views', `${__dirname}/dist`);
+server.set('view engine', 'mustache');
+server.engine('html', mustacheExpress());
+
+createEnvSettingsFile(path.resolve(`${__dirname}/dist/js/settings.js`));
+
+const renderApp = () =>
+    new Promise((resolve, reject) => {
+        server.render('index.html', {}, (err, html) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(html);
+            }
+        });
+    });
+
+const startServer = (html) => {
+    server.use('/dist/js', express.static(path.resolve(__dirname, 'dist/js')));
+    server.use('/dist/css', express.static(path.resolve(__dirname, 'dist/css')));
+
+    server.get('/health/isAlive', (req, res) => res.sendStatus(200));
+    server.get('/health/isReady', (req, res) => res.sendStatus(200));
+
+    server.get(/^\/(?!.*dist).*$/, (req, res) => {
+        res.send(html);
+    });
+
+    const port = process.env.PORT || 8082;
+    server.listen(port, () => {
+        console.log(`App listening on port: ${port}`);
+    });
+};
+
+const logError = (errorMessage, details) => console.log(errorMessage, details);
+
+renderApp().then(startServer, (error) => logError('Failed to render app', error));
