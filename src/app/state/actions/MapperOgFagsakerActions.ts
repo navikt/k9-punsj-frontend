705e5cb87b0258856d9acc38adb310539ecb0113
Tomas Engebretsen <tomas.engebretsen@nav.no>
2019-12-12 08:57:01 +0100

    Skjuler fagsaker

--- src/app/state/actions/MapperOgFagsakerActions.ts
+++ src/app/state/actions/MapperOgFagsakerActions.ts
@@ -65,7 +65,8 @@ export function findFagsaker(ident: string) {return (dispatch: any) => {
     dispatch(findFagsakerLoadAction(true));
     return get(ApiPath.FAGSAKER_FIND, {ident}, undefined,response => {
         if (response.ok) {
-            return response.json().then(r => dispatch(setFagsakerAction(r.fagsaker || [])));
+            // return response.json().then(r => dispatch(setFagsakerAction(r.fagsaker || [])));
+            return response.json().then(r => dispatch(setFagsakerAction([]))); // TODO: Avklare hvorvidt fagsaker skal vises eller ikke
         }
         return dispatch(findFagsakerErrorAction({
             status:     response.status,
