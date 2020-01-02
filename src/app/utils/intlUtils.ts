f08cb9db33aaf92044e5de72c0419998a1bb807a
Tomas Engebretsen <tomas.engebretsen@nav.no>
2020-01-02 08:35:42 +0100

    CSS-fiks og oppdatering av avhengigheter

--- src/app/utils/intlUtils.ts
+++ src/app/utils/intlUtils.ts
@@ -1,9 +1,3 @@
-import '@formatjs/intl-pluralrules/dist/locale-data/nb';
-import '@formatjs/intl-pluralrules/dist/locale-data/nn';
-import '@formatjs/intl-pluralrules/polyfill';
-import '@formatjs/intl-relativetimeformat/dist/locale-data/nb';
-import '@formatjs/intl-relativetimeformat/dist/locale-data/nn';
-import '@formatjs/intl-relativetimeformat/polyfill';
 import {IntlShape} from 'react-intl';
 
 const intlHelper = (intl: IntlShape, id: string, value?: {[key: string]: string}): string =>
