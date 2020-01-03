9bf2b0563f8e6490b287655141a072a3f0734c40
Tomas Engebretsen <tomas.engebretsen@nav.no>
2020-01-03 15:29:49 +0100

    Fjerner ting som ikke brukes

--- src/build/scripts/start-dev.js
+++ src/build/scripts/start-dev.js
@@ -2,7 +2,6 @@ const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const webpackConfig = require('../webpack/webpack.config.dev');
 const configureDevServer = require('../webpack/devserver.config');
-const getDecorator = require('./decorator');
 const path = require('path');
 const createEnvSettingsFile = require('./envSettings');
 
@@ -10,9 +9,8 @@ require('dotenv').config();
 
 createEnvSettingsFile(path.resolve(`${__dirname}/../../../dist/js/settings.js`));
 
-getDecorator().then((decoratorData) => {
-    const compiler = webpack(webpackConfig);
-    const server = new WebpackDevServer(compiler, configureDevServer(decoratorData));
+const compiler = webpack(webpackConfig);
+const server = new WebpackDevServer(compiler, configureDevServer({}));
+const port = 8080;
 
-    server.listen(8080, '127.0.0.1', () => console.log('Started server on http://localhost:8080'));
-});
+server.listen(port, '127.0.0.1', () => console.log(`Started server on http://localhost:${port}/`));
