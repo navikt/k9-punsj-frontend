c4b12c433c1d161ddcd701e5aed860ef0375f9ba
Anders Kristiansen <anders.kristiansen@nav.no>
2020-08-27 13:42:42 +0200

    TSF-1077 utvider med jest-dom assertions

--- src/test/testConfig.js
+++ src/test/testConfig.js
@@ -1,5 +1,6 @@
 /** Brukes i jest.config.json */
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import '@testing-library/jest-dom';
 
 configure({ adapter: new Adapter() });
