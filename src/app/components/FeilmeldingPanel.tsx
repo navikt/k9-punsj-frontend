2bde9a12998c51a61bd250f71252ba455105d829
alexandraban <alexandrabandarenka@nav.no>
2021-01-19 12:39:57 +0100

    css

--- src/app/components/FeilmeldingPanel.tsx
+++ src/app/components/FeilmeldingPanel.tsx
@@ -3,6 +3,8 @@ import {Undertittel} from "nav-frontend-typografi";
 import Panel from "nav-frontend-paneler";
 import {FormattedMessage} from "react-intl";
 
+import './feilmeldingPanel.less'
+
 export interface IFeilmeldingPanel {
     messageId: string
 }
