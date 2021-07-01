913cd57977ca1fb4c81a786020b66ca480b5e279
eras-dev <emma.rasmusson@nav.no>
2021-07-01 09:17:43 +0200

    fjerne initialvalues for nattevåk og beredskap

--- src/app/containers/pleiepenger/PSBPunchForm.tsx
+++ src/app/containers/pleiepenger/PSBPunchForm.tsx
@@ -849,10 +849,6 @@ export class PunchFormComponent extends React.Component<IPunchFormProps,
                     feilkodeprefiks={'beredskap'}
                     kanHaFlere={true}
                     medSlettKnapp={false}
-                    initialValues={{
-                        fom: soknad?.soeknadsperiode?.fom,
-                        tom: soknad?.soeknadsperiode?.tom
-                    }}
                 />
             )
         };
@@ -877,10 +873,6 @@ export class PunchFormComponent extends React.Component<IPunchFormProps,
                     feilkodeprefiks={'nattevåk'}
                     kanHaFlere={true}
                     medSlettKnapp={false}
-                    initialValues={{
-                        fom: soknad?.soeknadsperiode?.fom,
-                        tom: soknad?.soeknadsperiode?.tom
-                    }}
                 />
             )
         };
