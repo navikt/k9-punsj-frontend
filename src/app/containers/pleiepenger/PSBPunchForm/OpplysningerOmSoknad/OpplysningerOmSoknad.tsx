2b022c7bf0f07111c5acaecaaf230373eac2fd1d
eras-dev <emma.rasmusson@nav.no>
2021-09-09 16:06:11 +0200

    Lagt in informasjon vid opplysninger om söknad.

--- src/app/containers/pleiepenger/PSBPunchForm/OpplysningerOmSoknad/OpplysningerOmSoknad.tsx
+++ src/app/containers/pleiepenger/PSBPunchForm/OpplysningerOmSoknad/OpplysningerOmSoknad.tsx
@@ -4,9 +4,10 @@ import intlHelper from "../../../../utils/intlUtils";
 import {PunchFormPaneler} from "../../../../models/enums/PunchFormPaneler";
 import {Input, RadioPanelGruppe, SkjemaGruppe} from "nav-frontend-skjema";
 import {JaNeiIkkeRelevant} from "../../../../models/enums/JaNeiIkkeRelevant";
-import {AlertStripeAdvarsel} from "nav-frontend-alertstriper";
+import {AlertStripeAdvarsel, AlertStripeInfo} from "nav-frontend-alertstriper";
 import { PSBSoknad} from "../../../../models/types";
 import {IntlShape} from "react-intl";
+import './opplysningerOmSoknad.less';
 
 interface IOwnProps {
     intl: IntlShape;
@@ -25,48 +26,50 @@ const OpplysningerOmSoknad: React.FunctionComponent<IOwnProps> = ({
     signert,
     soknad
 }) => {
-    return ( <Panel className={"opplysningerOmSoknad"}>
-        <h3>{intlHelper(intl, PunchFormPaneler.OPPLYSINGER_OM_SOKNAD)}</h3>
-        <SkjemaGruppe>
-            <div className={"input-row"}>
-                <Input
-                    id="soknad-dato"
-                    bredde={"M"}
-                    label={intlHelper(intl, 'skjema.mottakelsesdato')}
-                    type="date"
-                    value={soknad.mottattDato}
-                    {...changeAndBlurUpdatesSoknad((event: any) => ({
-                        mottattDato: event.target.value,
+    return (
+        <Panel className={"opplysningerOmSoknad"}>
+            <h3>{intlHelper(intl, PunchFormPaneler.OPPLYSINGER_OM_SOKNAD)}</h3>
+            <AlertStripeInfo>{intlHelper(intl, 'skjema.mottakelsesdato.informasjon')}</AlertStripeInfo>
+            <SkjemaGruppe>
+                <div className={"input-row"}>
+                    <Input
+                        id="soknad-dato"
+                        bredde={"M"}
+                        label={intlHelper(intl, 'skjema.mottakelsesdato')}
+                        type="date"
+                        value={soknad.mottattDato}
+                        {...changeAndBlurUpdatesSoknad((event: any) => ({
+                            mottattDato: event.target.value,
+                        }))}
+                        feil={getErrorMessage('mottattDato')}
+                    />
+                    <Input
+                        value={soknad.klokkeslett || ''}
+                        type={"time"}
+                        className={"klokkeslett"}
+                        label={intlHelper(intl, 'skjema.mottatt.klokkeslett')}
+                        {...changeAndBlurUpdatesSoknad((event: any) => ({
+                            klokkeslett: event.target.value,
+                        }))}
+                        feil={getErrorMessage('klokkeslett')}
+                    />
+                </div>
+                <RadioPanelGruppe
+                    className="horizontalRadios"
+                    radios={Object.values(JaNeiIkkeRelevant).map((jn) => ({
+                        label: intlHelper(intl, jn),
+                        value: jn,
                     }))}
-                    feil={getErrorMessage('mottattDato')}
+                    name="signatur"
+                    legend={intlHelper(intl, 'ident.signatur.etikett')}
+                    checked={signert || undefined}
+                    onChange={(event) =>
+                        setSignaturAction(((event.target as HTMLInputElement).value as JaNeiIkkeRelevant) || null)
+                    }
                 />
-                <Input
-                    value={soknad.klokkeslett || ''}
-                    type={"time"}
-                    className={"klokkeslett"}
-                    label={intlHelper(intl, 'skjema.mottatt.klokkeslett')}
-                    {...changeAndBlurUpdatesSoknad((event: any) => ({
-                        klokkeslett: event.target.value,
-                    }))}
-                    feil={getErrorMessage('klokkeslett')}
-                />
-            </div>
-            <RadioPanelGruppe
-                className="horizontalRadios"
-                radios={Object.values(JaNeiIkkeRelevant).map((jn) => ({
-                    label: intlHelper(intl, jn),
-                    value: jn,
-                }))}
-                name="signatur"
-                legend={intlHelper(intl, 'ident.signatur.etikett')}
-                checked={signert || undefined}
-                onChange={(event) =>
-                    setSignaturAction(((event.target as HTMLInputElement).value as JaNeiIkkeRelevant) || null)
-                }
-            />
-            {signert === JaNeiIkkeRelevant.NEI &&
-            <AlertStripeAdvarsel>{intlHelper(intl, 'skjema.usignert.info')}</AlertStripeAdvarsel>}
-        </SkjemaGruppe>
-    </Panel>)
+                {signert === JaNeiIkkeRelevant.NEI &&
+                <AlertStripeAdvarsel>{intlHelper(intl, 'skjema.usignert.info')}</AlertStripeAdvarsel>}
+            </SkjemaGruppe>
+        </Panel>)
 }
 export default OpplysningerOmSoknad;
