94d3140443f4ba1ff037bca02731b3674dbbb6b9
eras-dev <emma.rasmusson@nav.no>
2021-09-16 11:02:38 +0200

    fjernet envSettings tidigare oppsett, heroku-build og drog ut envSettings i egen fil slik att den kan brukes av dev lokalt og prod

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,6 @@ COPY dist ./dist
 COPY server.js .
 COPY node_modules ./node_modules
 COPY package.json .
-COPY src/build/scripts/envSettings.js ./src/build/scripts/envSettings.js
 
 EXPOSE 8080
 CMD ["npm", "run", "start-express"]
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mustacheExpress = require('mustache-express');
 const Promise = require('promise');
 const compression = require('compression');
 const helmet = require('helmet');
+const envSettings = require('./src/build/envSettings');
 
 const server = express();
 server.use(helmet({
@@ -36,15 +37,6 @@ const renderApp = () =>
           }
       });
   });
-
-const envSettings = () => {
-    const appSettings = `window.appSettings = {OIDC_AUTH_PROXY: '${process.env.OIDC_AUTH_PROXY}', K9_LOS_URL: '${process.env.K9_LOS_URL}'};`.trim().replace(/ /g, '');
-    try {
-        return appSettings;
-    } catch (e) {
-        console.error(e);
-    }
-}
 
 const startServer = (html) => {
     server.get(`/settings/settings.js`, (req, res) => {
