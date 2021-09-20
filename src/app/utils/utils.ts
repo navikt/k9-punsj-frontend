b822657ef49c3b8e2ea7e98c05b8cc480ad2879c
vebnor <vebjorn.nordby@hotmail.com>
2021-09-20 10:40:46 +0200

    flytter funksjoner

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -1,22 +1,27 @@
-import {ICountry} from "../components/country-select/CountrySelect";
-
+import { ICountry } from '../components/country-select/CountrySelect';
 
 export const formattereDatoIArray = (dato: number[]) => {
-    const formatertDato: string[] = [];
-    for (let i = dato.length - 1; i >= 0; i--) {
-        formatertDato.push(i > 0 ? `${dato[i]}.` : `${dato[i]}`)
-    }
-    return formatertDato.join('');
+  const formatertDato: string[] = [];
+  for (let i = dato.length - 1; i >= 0; i--) {
+    formatertDato.push(i > 0 ? `${dato[i]}.` : `${dato[i]}`);
+  }
+  return formatertDato.join('');
 };
 
-export const sjekkPropertyEksistererOgIkkeErNull = (property: string, object: any) => {
-    if (property in object && object[property] !== null) {
-        return true
-    }
-    return false
+export const sjekkPropertyEksistererOgIkkeErNull = (
+  property: string,
+  object: any
+) => {
+  if (property in object && object[property] !== null) {
+    return true;
+  }
+  return false;
 };
 
-export const formattereLandTilNavn = (landskode: string, countryList: ICountry[]) => {
-    const landNavn = countryList.find(country => country.code === landskode);
-    return typeof landNavn !== undefined ? landNavn?.name : '';
+export const formattereLandTilNavn = (
+  landskode: string,
+  countryList: ICountry[]
+) => {
+  const landNavn = countryList.find((country) => country.code === landskode);
+  return typeof landNavn !== undefined ? landNavn?.name : '';
 };
