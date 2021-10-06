3d4a39daefeb425cf5921dbc9be5d305dad55f2c
vebnor <vebjorn.nordby@nav.no>
2021-10-06 12:58:21 +0200

    intl hook

--- src/app/containers/pleiepenger/UtregningArbeidstid.tsx
+++ src/app/containers/pleiepenger/UtregningArbeidstid.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { IntlShape } from 'react-intl';
+import { useIntl } from 'react-intl';
 import intlHelper from 'app/utils/intlUtils';
 
 interface IUtregningArbeidstidProps {
     arbeidstid?: string;
     normalArbeidstid?: string;
-    intl: IntlShape;
 }
 
-const UtregningArbeidstid = ({ arbeidstid, normalArbeidstid, intl }: IUtregningArbeidstidProps): JSX.Element | null => {
+const UtregningArbeidstid = ({ arbeidstid, normalArbeidstid }: IUtregningArbeidstidProps): JSX.Element | null => {
     const convert = (tid: string) => Number(tid.replace(',', '.'));
     const regnUt = (tid: string, tidTilDeling: string) =>
         ((convert(tid) / convert(tidTilDeling)) * 100).toLocaleString(undefined, {
@@ -17,6 +16,8 @@ const UtregningArbeidstid = ({ arbeidstid, normalArbeidstid, intl }: IUtregningA
         });
 
     const skalRegneProsent = normalArbeidstid && !Number.isNaN(Number(normalArbeidstid));
+    const intl = useIntl();
+    
 
     if (!arbeidstid || Number.isNaN(convert(arbeidstid))) {
         return null;
