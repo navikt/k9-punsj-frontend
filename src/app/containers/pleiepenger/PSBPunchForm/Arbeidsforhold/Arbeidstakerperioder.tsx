aa988f9f9539c2d67e9c6c919b3d9bb624830736
vebnor <vebjorn.nordby@nav.no>
2021-10-06 12:52:38 +0200

    fjerner ubrukt import

--- src/app/containers/pleiepenger/PSBPunchForm/Arbeidsforhold/Arbeidstakerperioder.tsx
+++ src/app/containers/pleiepenger/PSBPunchForm/Arbeidsforhold/Arbeidstakerperioder.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import AddCircleSvg from 'app/assets/SVG/AddCircleSVG';
 import BinSvg from 'app/assets/SVG/BinSVG';
-import Feilmelding from 'app/components/Feilmelding';
 import { ArbeidsgivereResponse } from 'app/models/types/ArbeidsgivereResponse';
 import Organisasjon from 'app/models/types/Organisasjon';
 import { get } from 'app/utils';
