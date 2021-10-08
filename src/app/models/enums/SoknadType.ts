b3feeb8804b42c6388e2d6ebcf69a84089547208
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2021-10-08 10:49:30 +0200

    Sletter filer som ikke er i bruk (#888)

--- src/app/models/enums/SoknadType.ts
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export const SoknadType = {
-    PSB: 'Pleiepenger sykt barn',
-    'omsorgspenger-overføring': 'Overføring av omsorgsdager',
-    'omsorgspenger-fordeling': 'Deling av omsorgsdager',
-};
