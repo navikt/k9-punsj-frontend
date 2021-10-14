11fd6096f98dc91e61b9f5e2db8b10bd58b2dca2
vebnor <vebjorn.nordby@nav.no>
2021-10-14 09:12:52 +0200

    tilpasser perioder til utleding av feilmelding

--- src/app/components/skjema/skjemaUtils.ts
+++ src/app/components/skjema/skjemaUtils.ts
@@ -6,4 +6,9 @@ export const fjernIndexFraLabel = (label: string) => label.replace(/\[.*\]/g, '[
 export const generateDateString = (p: IPeriode | null) =>
     p ? `${moment(p.fom).format('DD.MM.YYYY')} - ${moment(p.tom).format('DD.MM.YYYY')}` : '-';
 
-export const periodeSpenn = ((periode: IPeriode | undefined): string => periode ? Object.values(periode).join('/') : '')
+export const periodeSpenn = (periode: IPeriode | undefined): string =>
+    periode
+        ? Object.values(periode)
+            .map((dato) => dato || '..')
+            .join('/')
+        : '';
