682f4781967dbd680feb2c66d19811d9511bf98a
Hallvard Andreas Stark <hallvard@scel.to>
2021-11-01 21:32:58 +0100

    Fungerende innsending av søknad Viser bekreftelsemodal med kvittering

--- src/app/models/types/OMSSoknadUt.ts
+++ src/app/models/types/OMSSoknadUt.ts
@@ -2,7 +2,7 @@ import { KorrigeringAvInntektsmeldingFormValues } from 'app/containers/omsorgspe
 import { IPeriode } from './Periode';
 
 export interface Fravaersperioder {
-    periode: IPeriode[];
+    periode: IPeriode;
     faktiskTidPrDag: null | string;
 }
 
@@ -10,23 +10,29 @@ const lagFraværsperioder = (values: KorrigeringAvInntektsmeldingFormValues) => 
     const fraværsperioder: Fravaersperioder[] = [];
 
     if (values.Trekkperioder.length > 0 && values.Trekkperioder[0].fom) {
-        fraværsperioder.push({
-            periode: values.Trekkperioder,
-            faktiskTidPrDag: '0',
+        values.Trekkperioder.forEach((trekkperiode) => {
+            fraværsperioder.push({
+                periode: trekkperiode,
+                faktiskTidPrDag: '0',
+            });
         });
     }
     if (values.PerioderMedRefusjonskrav.length > 0 && values.PerioderMedRefusjonskrav[0].fom) {
-        fraværsperioder.push({
-            periode: values.PerioderMedRefusjonskrav,
-            faktiskTidPrDag: null,
+        values.PerioderMedRefusjonskrav.forEach((periodeMedRefusjonskrav) => {
+            fraværsperioder.push({
+                periode: periodeMedRefusjonskrav,
+                faktiskTidPrDag: null,
+            });
         });
     }
     if (values.DagerMedDelvisFravær.length > 0 && values.DagerMedDelvisFravær[0].dato) {
         values.DagerMedDelvisFravær.forEach((dagMedDelvisFravær) => {
-            fraværsperioder.push({
-                periode: [{ fom: dagMedDelvisFravær.dato, tom: dagMedDelvisFravær.dato }],
-                faktiskTidPrDag: dagMedDelvisFravær.timer,
-            });
+            if (dagMedDelvisFravær.dato && dagMedDelvisFravær.timer) {
+                fraværsperioder.push({
+                    periode: { fom: dagMedDelvisFravær.dato, tom: dagMedDelvisFravær.dato },
+                    faktiskTidPrDag: dagMedDelvisFravær.timer,
+                });
+            }
         });
     }
 
