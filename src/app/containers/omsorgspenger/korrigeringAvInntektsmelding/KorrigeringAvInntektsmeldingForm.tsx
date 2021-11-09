fbd007cc190eac819208baa8ffc90a868d296fba
Hallvard Andreas Stark <hallvard@scel.to>
2021-11-09 09:35:04 +0100

    Håndtering av feilet innsending av data

--- src/app/containers/omsorgspenger/korrigeringAvInntektsmelding/KorrigeringAvInntektsmeldingForm.tsx
+++ src/app/containers/omsorgspenger/korrigeringAvInntektsmelding/KorrigeringAvInntektsmeldingForm.tsx
@@ -89,23 +89,23 @@ const KorrigeringAvInntektsmeldingForm: React.FC<KorrigeringAvInntektsmeldingFor
     };
 
     const sendInnSøknad = (formVerdier: KorrigeringAvInntektsmeldingFormValues) => {
-        // setIsLoading(true);
         submitOMSSoknad(søkerId, søknadId, (response, responseData) => {
             switch (response.status) {
-                case 202: {
+                case 202:
                     dispatch({ type: ActionType.SET_SØKNAD_INNSENDT, innsendteFormverdier: formVerdier });
                     break;
-                }
-                case 400: {
-                    console.log('400');
+                case 400:
+                case 409:
+                    dispatch({
+                        type: ActionType.SET_FORM_ERROR,
+                        formError: 'Innsending av korrigering feilet',
+                    });
                     break;
-                }
-                case 409: {
-                    console.log('409');
-                    break;
-                }
                 default: {
-                    console.log('default??');
+                    dispatch({
+                        type: ActionType.SET_FORM_ERROR,
+                        formError: 'Innsending av korrigering feilet',
+                    });
                 }
             }
         });
