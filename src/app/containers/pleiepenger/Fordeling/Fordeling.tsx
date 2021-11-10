67e3f540537589e4718dc0c5b1a30d49fcc12394
vebnor <vebjorn.nordby@nav.no>
2021-11-10 11:13:21 +0100

    Revert "revert"
    This reverts commit 545a365c021a20b56a7bcac5f5edc3f20e8c6261.

--- src/app/containers/pleiepenger/Fordeling/Fordeling.tsx
+++ src/app/containers/pleiepenger/Fordeling/Fordeling.tsx
@@ -101,7 +101,6 @@ const FordelingComponent: React.FunctionComponent<IFordelingProps> = (props: IFo
     );
     const konfigForValgtSakstype = useMemo(() => sakstyper.find((st) => st.navn === sakstype), [sakstype]);
 
-    const [omsorgspengerValgt, setOmsorgspengerValgt] = useState<boolean>(false);
     const [barnetHarIkkeFnr, setBarnetHarIkkeFnr] = useState<boolean>(false);
 
     const [dokumenttype, setDokumenttype] = useState<FordelingDokumenttype>();
@@ -392,7 +391,6 @@ const FordelingComponent: React.FunctionComponent<IFordelingProps> = (props: IFo
                             journalpost={journalpost}
                             erJournalfoertEllerFerdigstilt={erJournalfoertEllerFerdigstilt}
                             kanJournalforingsoppgaveOpprettesiGosys={kanJournalforingsoppgaveOpprettesiGosys}
-                            setOmsorgspengerValgt={setOmsorgspengerValgt}
                             identState={identState}
                             konfigForValgtSakstype={konfigForValgtSakstype}
                             fordelingState={fordelingState}
