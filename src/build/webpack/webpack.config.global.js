be0838222bd53981198a757796d4f3f9ac4997dd
vebnor <vebjorn.nordby@nav.no>
2021-11-10 10:15:05 +0100

    prøver å fjerne defaultverdi for NODE_ENV

--- src/build/webpack/webpack.config.global.js
+++ src/build/webpack/webpack.config.global.js
@@ -78,7 +78,6 @@ const webpackConfig = {
         new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /nb|nn|en/),
         new CopyPlugin({ patterns: [{ from: 'src/app/favicon.png' }] }),
         new webpack.EnvironmentPlugin({
-            NODE_ENV: 'development',
             OIDC_AUTH_PROXY: null,
             K9_LOS_URL: 'http://localhost:8030',
         }),
