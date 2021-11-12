dd75e53401aa29064c24b581bd4900ec513af923
vebnor <vebjorn.nordby@nav.no>
2021-11-12 22:20:12 +0100

    fjerner logging

--- src/app/state/reducers/FellesReducer.ts
+++ src/app/state/reducers/FellesReducer.ts
@@ -131,7 +131,6 @@ export function getJournalpost(journalpostid: string) {
     return (dispatch: any) => {
         dispatch(getJournalpostLoadAction());
         return get(ApiPath.JOURNALPOST_GET, { journalpostId: journalpostid }, undefined, (response, data) => {
-            console.log(data)
             if (response.ok) {
                 return dispatch(setJournalpostAction(data));
             }
