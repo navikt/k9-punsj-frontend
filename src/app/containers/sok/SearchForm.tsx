abd77fa3561e0a082b1cab21293934fa1a92698d
Hallvard Andreas Stark <hallvard@scel.to>
2021-11-17 14:15:22 +0100

    forsøk på å trigge error i sentry

--- src/app/containers/sok/SearchForm.tsx
+++ src/app/containers/sok/SearchForm.tsx
@@ -165,6 +165,15 @@ export class SearchFormComponent extends React.Component<ISearchFormProps, ISear
                         </AlertStripeAdvarsel>
                     )}
                 </SkjemaGruppe>
+                <button
+                    type="button"
+                    style={{ display: 'none' }}
+                    onClick={() => {
+                        throw new Error();
+                    }}
+                >
+                    Ødelegg
+                </button>
             </div>
         );
     }
