47a749a7d0c15019f61711d5bbada0fee0ba82f0
Hallvard Andreas Stark <hallvard@scel.to>
2021-11-24 10:10:24 +0100

    Retter test

--- cypress/integration/fordeling/Fordeling.spec.js
+++ cypress/integration/fordeling/Fordeling.spec.js
@@ -2,13 +2,14 @@ describe('Fordeling', () => {
     beforeEach(() => {
         cy.visit('/journalpost/200');
     });
-    it.skip('viser dokumentvalg', () => {
-        cy.contains(/Gjelder dokumentet Pleiepenger sykt barn?/i).should('exist');
-        cy.contains('Ja').should('exist');
-        cy.contains('Nei').should('exist');
+    it('viser dokumentvalg', () => {
+        cy.contains(/Dette gjelder:?/i).should('exist');
+        cy.contains('Pleiepenger').should('exist');
+        cy.contains('Korrigering av inntektsmelding omsorgspenger AG').should('exist');
+        cy.contains('Annet').should('exist');
     });
 
-    it.skip('kan opprette journalføringsoppgave i Gosys', () => {
+    it('kan opprette journalføringsoppgave i Gosys', () => {
         cy.intercept(
             {
                 method: 'GET',
@@ -16,7 +17,7 @@ describe('Fordeling', () => {
             },
             { fixture: 'gosysKategorier.json' }
         ).as('gosysKategorier');
-        cy.contains('Nei').click();
+        cy.contains('Annet').click();
         cy.wait('@gosysKategorier');
         const identifikatorInput = cy.findByLabelText(/Søkers fødselsnummer eller D-nummer/i).should('exist');
         identifikatorInput.clear().type('13337');
