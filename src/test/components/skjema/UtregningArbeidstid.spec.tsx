0e061ea50e26172e1a4e29743f1df45b7b9c2142
Vebjørn Nordby <vebjorn.nordby@nav.no>
2021-11-29 12:57:52 +0100

    Skjuler utregning av arbeidstid hvis den ikke er tall (#985)
    * skal ikke vise prosentvis utregning av arbeidstid hvis den er NaN eller ∞
    
    * tekst

---
+++ src/test/components/skjema/UtregningArbeidstid.spec.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import UtregningArbeidstid from 'app/containers/pleiepenger/UtregningArbeidstid';
+import { renderWithIntl } from '../../testUtils';
+
+jest.mock('app/utils/envUtils');
+
+describe('utregning av arbeidstid', () => {
+    test('viser timer per uke', () => {
+        renderWithIntl(<UtregningArbeidstid arbeidstid="7,5" />);
+
+        expect(screen.getByText('= 37.5 timer per uke')).toBeInTheDocument();
+    });
+    test('viser prosent av normal arbeidstid', () => {
+        renderWithIntl(<UtregningArbeidstid arbeidstid="5" normalArbeidstid="7,5" />);
+
+        expect(screen.getByText('= 25 timer per uke')).toBeInTheDocument();
+        expect(screen.getByText('(tilsvarer 66.667% arbeid)')).toBeInTheDocument();
+    });
+    test('skjuler prosent av normal arbeidstid når utregning er ∞', () => {
+        renderWithIntl(<UtregningArbeidstid arbeidstid="1" normalArbeidstid="0" />);
+
+        expect(screen.getByText('= 5 timer per uke')).toBeInTheDocument();
+        expect(screen.queryByText('(tilsvarer ∞% arbeid)')).not.toBeInTheDocument();
+    });
+    test('skjuler prosent av normal arbeidstid når utregning er NaN', () => {
+        renderWithIntl(<UtregningArbeidstid arbeidstid="0" normalArbeidstid="0" />);
+
+        expect(screen.getByText('= 0 timer per uke')).toBeInTheDocument();
+        expect(screen.queryByText('(tilsvarer NaN% arbeid)')).not.toBeInTheDocument();
+    });
+});
