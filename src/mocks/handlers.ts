9873b94d0f2a2e83919f681c4769426772da9fe5
Hallvard Andreas Stark <hallvard@scel.to>
2022-03-17 15:21:39 +0100

    Fullførte test Flytte tekst til intl

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -56,7 +56,7 @@ let handlers = [
     rest.get('http://localhost:8101/api/k9-punsj/saker/hent', (req, res, ctx) =>
         res(
             ctx.status(200),
-            ctx.delay(1000),
+            ctx.delay(500),
             ctx.json([
                 {
                     fagsakId: '1DMU93M',
@@ -74,7 +74,7 @@ let handlers = [
         )
     ),
     rest.post('http://localhost:8101/api/k9-punsj/notat/opprett', (req, res, ctx) =>
-        res(ctx.status(201), ctx.delay(1000), ctx.json({ journalpostId: '200' }))
+        res(ctx.status(201), ctx.delay(500), ctx.json({ journalpostId: '200' }))
     ),
 ];
 
