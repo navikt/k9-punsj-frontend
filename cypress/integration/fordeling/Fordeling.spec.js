9761e32216ab7b1c2a868dda28d5986068fd04f8
vebnor <vebjorn.nordby@nav.no>
2022-05-30 14:13:32 +0200

    legger til test for å se om man kommer inn i skjema for korrigering av inntektsmelding

--- cypress/integration/fordeling/Fordeling.spec.js
+++ cypress/integration/fordeling/Fordeling.spec.js
@@ -1,3 +1,6 @@
+import { ApiPath } from 'app/apiConfig';
+import { BACKEND_BASE_URL } from '../../../src/mocks/konstanter';
+
 describe('Fordeling', () => {
     beforeEach(() => {
         cy.visit('/journalpost/200');
@@ -21,5 +24,26 @@ describe('Fordeling', () => {
         cy.wait('@gosysKategorier');
         const identifikatorInput = cy.findByLabelText(/Søkers fødselsnummer eller D-nummer/i).should('exist');
         identifikatorInput.clear().type('13337');
+    });
+    it('kan korrigere inntektsmelding uten at sjekkSkalTilK9 kjøres', () => {
+        cy.window().then((window) => {
+            const { worker, rest } = window.msw;
+            worker.use(
+                rest.get(`${BACKEND_BASE_URL}/api/k9-punsj${ApiPath.SJEKK_OM_SKAL_TIL_K9SAK}`, (req, res, ctx) =>
+                    res(
+                        ctx.json({
+                            k9sak: false,
+                        })
+                    )
+                )
+            );
+        });
+
+        cy.findByText(/Korrigering av inntektsmelding omsorgspenger AG/i).click();
+        cy.findByText('Ja').click();
+        cy.findByRole('button', { name: /Videre/i }).click();
+        cy.findByText('Korrigere/trekke refusjonskrav omsorgspenger').click();
+        cy.findByRole('button', { name: /bekreft/i }).click();
+        cy.url().should('eq', 'http://localhost:8080/journalpost/200#/korrigering-av-inntektsmelding');
     });
 });
