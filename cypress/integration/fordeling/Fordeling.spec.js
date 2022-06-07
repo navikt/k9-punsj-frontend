42fa821eb74e4c1ee4db4adff1712e2f79c852ec
eras-dev <emma.rasmusson@nav.no>
2022-06-07 11:26:11 +0200

    lagt till mer tester

--- cypress/integration/fordeling/Fordeling.spec.js
+++ cypress/integration/fordeling/Fordeling.spec.js
@@ -22,4 +22,31 @@ describe('Fordeling', () => {
         const identifikatorInput = cy.findByLabelText(/Søkers fødselsnummer eller D-nummer/i).should('exist');
         identifikatorInput.clear().type('13337');
     });
+
+    it('validering av fødselsnummer virker', () => {
+        cy.contains('Pleiepenger').click();
+        cy.contains('Nei').click();
+
+        const identifikatorInput = cy.findByLabelText(/Søkers fødselsnummer eller D-nummer:/i)
+          .should('exist');
+
+        identifikatorInput.clear()
+          .type('28887298663');
+
+        cy.findByText(/Dette er ikke et gyldig fødsels- eller D-nummer./i).should("not.exist");
+    })
+
+    it('validering av fødselsnummer IKKE virker', () => {
+        cy.contains('Pleiepenger').click();
+        cy.contains('Nei').click();
+
+        const identifikatorInput = cy.findByLabelText(/Søkers fødselsnummer eller D-nummer:/i)
+          .should('exist');
+
+        identifikatorInput.clear()
+          .type('2888729866')
+          .blur();
+
+        cy.findByText(/Dette er ikke et gyldig fødsels- eller D-nummer./i).should("exist");
+    })
 });
