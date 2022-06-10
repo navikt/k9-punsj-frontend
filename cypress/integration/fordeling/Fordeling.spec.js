5f4476317c09440104a73375c678d3b288444c8a
eras-dev <emma.rasmusson@nav.no>
2022-06-10 14:53:23 +0200

    cypress tester

--- cypress/integration/fordeling/Fordeling.spec.js
+++ cypress/integration/fordeling/Fordeling.spec.js
@@ -2,11 +2,22 @@ describe('Fordeling', () => {
     beforeEach(() => {
         cy.visit('/journalpost/200');
     });
+
     it('viser dokumentvalg', () => {
         cy.contains(/Dette gjelder:?/i).should('exist');
         cy.contains('Pleiepenger').should('exist');
-        cy.contains('Korrigering av inntektsmelding omsorgspenger AG').should('exist');
+        cy.contains('Omsorgspenger/omsorgsdager').should('exist');
+        cy.contains('Pleiepenger i livets sluttfase').should('exist');
         cy.contains('Annet').should('exist');
+    });
+
+    it('viser subdokumentvalg omsorgspenger', () => {
+        cy.contains('Ekstra omsorgsdager ved kronisk sykt eller funksjonshemmet barn').should('not.exist');
+        cy.contains('Korrigering av inntektsmelding omsorgspenger AG').should('not.exist');
+
+        cy.contains('Omsorgspenger/omsorgsdager').should('exist').click();
+        cy.contains('Ekstra omsorgsdager ved kronisk sykt eller funksjonshemmet barn').should('exist');
+        cy.contains('Korrigering av inntektsmelding omsorgspenger AG').should('exist').click()
     });
 
     it('kan opprette journalføringsoppgave i Gosys', () => {
