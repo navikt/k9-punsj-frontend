a6234b7d7420f06d70bf3d3361c3128460687112
eras-dev <76997730+eras-dev@users.noreply.github.com>
2022-06-14 10:16:35 +0200

    Feat/stötte for test norge fnr i q (#1200)
    * test
    
    * refaktorering: Har tagit bort funksjoner som ikke bruker når vi bruker det nye biblioteket og endret navn samt logikk
    
    * lagt till mer tester
    
    * rydde
    
    Co-authored-by: vebnor <vebjorn.nordby@nav.no>

--- src/app/rules/IdentRules.ts
+++ src/app/rules/IdentRules.ts
@@ -1,94 +1,21 @@
+import validator from '@navikt/fnrvalidator';
+
 // eslint-disable-next-line import/prefer-default-export
 export class IdentRules {
-    public static REGEX_FNR =
-        /^((((0[1-9]|[12]\d|30)(0[469]|11)|(0[1-9]|[12]\d|3[01])(0[13578]|1[02])|((0[1-9]|1\d|2[0-8])02))\d{2})|2902([02468][048]|[13579][26]))\d{5}$/;
+    public static erUgyldigIdent = (ident: string | null): boolean => {
+        if (!ident || !ident.length) return true;
 
-    public static REGEX_DNR =
-        /^((((4[1-9]|[56]\d|70)(0[469]|11)|(4[1-9]|[56]\d|7[01])(0[13578]|1[02])|((4[1-9]|5\d|6[0-8])02))\d{2})|6902([02468][048]|[13579][26]))\d{5}$/;
+        const { status } = validator.idnr(ident);
 
-    public static CONTROL_KEY_1 = [3, 7, 6, 1, 8, 9, 4, 5, 2];
+        return status === 'invalid';
+    };
 
-    public static CONTROL_KEY_2 = [5, 4, 3, 2, 7, 6, 5, 4, 3, 2];
+    public static erAlleIdenterGyldige = (ident1: string, ident2: string | null): boolean => {
+        if (!ident1) return false;
+        if (!ident2) return !IdentRules.erUgyldigIdent(ident1);
 
-    public static hasIdent11Digits(ident?: any) {
-        return typeof ident === 'string' ? /^\d{11}$/.test(ident) : false;
-    }
+        return !IdentRules.erUgyldigIdent(ident1) && !IdentRules.erUgyldigIdent(ident2);
+    };
 
-    public static isIdentValid(ident: string): boolean {
-        return this.isIdentFormatValid(ident) && this.areControlDigitsValid(ident);
-    }
-
-    public static isIdentFormatValid(ident: string) {
-        return this.REGEX_FNR.test(ident) || this.REGEX_DNR.test(ident);
-    }
-
-    public static areControlDigitsValid(ident: string) {
-        return this.isControlDigit1Valid(ident) && this.isControlDigit2Valid(ident);
-    }
-
-    public static areIdentsValid(ident1?: any, ident2?: any, annenSokerIdent?: any): boolean {
-        if (typeof ident1 === 'string') {
-            if (typeof ident2 === 'string' && ident2 !== '') {
-                return this.isIdentValid(ident1) && this.isIdentValid(ident2);
-            }
-            if (!ident2) {
-                return this.isIdentValid(ident1);
-            }
-            if (typeof annenSokerIdent === 'string' && annenSokerIdent !== '') {
-                return this.isIdentValid(annenSokerIdent);
-            }
-            return false;
-        }
-        return false;
-    }
-
-    public static controlDigit1(ident: string): number | null {
-        if (!this.hasIdent11Digits(ident)) {
-            return null;
-        }
-
-        const d = (index: number) => Number(ident!.charAt(index));
-        const result = 11 - (this.CONTROL_KEY_1.reduce((s, f, i) => s + f * d(i), 0) % 11);
-
-        switch (result) {
-            case 11:
-                return 0;
-            case 10:
-                return null;
-            default:
-                return result;
-        }
-    }
-
-    public static isControlDigit1Valid(ident: string): boolean {
-        if (!this.hasIdent11Digits(ident)) {
-            return false;
-        }
-        return Number(ident.charAt(9)) === this.controlDigit1(ident);
-    }
-
-    public static controlDigit2(ident: string): number | null {
-        if (!this.hasIdent11Digits(ident)) {
-            return null;
-        }
-
-        const d = (index: number) => Number(ident!.charAt(index));
-        const result = 11 - (this.CONTROL_KEY_2.reduce((s, f, i) => s + f * d(i), 0) % 11);
-
-        switch (result) {
-            case 11:
-                return 0;
-            case 10:
-                return null;
-            default:
-                return result;
-        }
-    }
-
-    public static isControlDigit2Valid(ident: string): boolean {
-        if (!this.hasIdent11Digits(ident)) {
-            return false;
-        }
-        return Number(ident.charAt(10)) === this.controlDigit2(ident);
-    }
+    public static harFnr11Siffrer = (ident?: any) => typeof ident === 'string' ? /^\d{11}$/.test(ident) : false
 }
