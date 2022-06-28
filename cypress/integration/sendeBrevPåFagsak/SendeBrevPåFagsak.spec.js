e4d7d01fe8069a588266afc69dd6e298c18e66af
Hallvard Andreas Stark <hallvard@scel.to>
2022-06-28 13:59:39 +0200

    Test av navigasjon til sending av brev på fagsak

---
+++ cypress/integration/sendeBrevPåFagsak/SendeBrevPåFagsak.spec.js
@@ -0,0 +1,15 @@
+describe('Send brev på fagsak', () => {
+    beforeEach(() => {
+        cy.visit('/journalpost/200');
+    });
+
+    it('skal navigere til visning', () => {
+        cy.contains('Omsorgspenger/omsorgsdager').should('exist').click();
+        cy.contains('Ekstra omsorgsdager ved kronisk sykt eller funksjonshemmet barn').should('exist').click();
+        cy.findByLabelText(/Velg fagsak/i).select('1DMU93M (K9 Pleiepenger sykt barn)');
+        cy.findByRole('button', { name: /Videre/i }).click();
+        cy.findByText(/Send brev og lukk oppgave i LOS/i).click();
+        cy.findByRole('button', { name: /bekreft/i }).click();
+        cy.url().should('eq', 'http://localhost:8080/journalpost/200#/send-brev-fagsak');
+    });
+});
