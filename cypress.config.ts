e190e0b188f2f07cbcf8d29de045dd86c78a0295
vebnor <vebjorn.nordby@nav.no>
2023-04-12 11:56:28 +0200

    skru på test isolation

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  video: false,
-  e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+    video: false,
+    e2e: {
+        // We've imported your old cypress plugins here.
+        // You may want to clean this up later by importing these.
+        setupNodeEvents(on, config) {
+            return require('./cypress/plugins/index.js')(on, config);
+        },
+        testIsolation: true,
+        baseUrl: 'http://localhost:8080',
+        specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     },
-    baseUrl: 'http://localhost:8080',
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
-  },
-})
+});
