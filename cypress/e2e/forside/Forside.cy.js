890d0bf75e2dfe97eeb2b8635c6a8b1fc8029ba0
vebnor <vebjorn.nordby@nav.no>
2023-05-10 13:00:16 +0200

    kanskje fikse tester

--- cypress/e2e/forside/Forside.cy.js
+++ cypress/e2e/forside/Forside.cy.js
@@ -1,18 +1,13 @@
 import { LOCAL_API_URL } from '../../../src/mocks/konstanter';
 
 describe('forside', () => {
-    onBeforeLoad: (window) => {
-        window.__initialState__ = initialState;
-    },
-        it('kan søke opp journalpost', () => {
-            cy.visit('/', {
-                onBeforeLoad: (window) => {
-                    window.__initialState__ = initialState;
-                },
-            });
-            cy.soekPaaJournalpost();
-            cy.url().should('contains', '/journalpost/200#/');
-        });
+    cy.visit('/', {
+        onBeforeLoad: (window) => {},
+    });
+    it('kan søke opp journalpost', () => {
+        cy.soekPaaJournalpost();
+        cy.url().should('contains', '/journalpost/200#/');
+    });
 
     it('får feilmelding når journalposten ikke finnes', () => {
         cy.window().then((window) => {
@@ -20,7 +15,6 @@ describe('forside', () => {
             worker.use(rest.get(`${LOCAL_API_URL}/journalpost/201`, (req, res, ctx) => res(ctx.status(404))));
         });
 
-        cy.visit('/');
         cy.soekPaaJournalpost('201');
         cy.contains(/Det finnes ingen journalposter med ID 201/i).should('exist');
     });
@@ -30,11 +24,6 @@ describe('forside', () => {
             const { worker } = window.msw;
             worker.use(rest.get(`${LOCAL_API_URL}/journalpost/203`, (req, res, ctx) => res(ctx.status(404))));
         });
-        cy.intercept('GET', '/api/k9-punsj/journalpost/203', {
-            statusCode: 403,
-        });
-
-        cy.visit('/');
         cy.soekPaaJournalpost('203');
         cy.contains(/Du har ikke tilgang til å slå opp denne personen/i).should('exist');
     });
