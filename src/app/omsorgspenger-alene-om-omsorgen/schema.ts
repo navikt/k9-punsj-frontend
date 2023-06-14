ae558c5930a1f952aa8b52b1b92269569b8ad9b0
vebnor <vebjorn.nordby@nav.no>
2023-06-14 14:45:20 +0200

    legger til signatur i metadata

--- src/app/omsorgspenger-alene-om-omsorgen/schema.ts
+++ src/app/omsorgspenger-alene-om-omsorgen/schema.ts
@@ -1,10 +1,12 @@
 import yup, { barn, passertDato, passertKlokkeslettPaaMottattDato, periode } from 'app/rules/yup';
 
 const OMPAOSchema = yup.object({
+    metadata: yup.object({
+        signatur: yup.string().required().label('Signatur'),
+    }),
     mottattDato: passertDato,
     klokkeslett: passertKlokkeslettPaaMottattDato,
     begrunnelseForInnsending: yup.string(),
-    metadata: yup.object().required(),
     soeknadsperiode: periode(),
     barn,
 });
