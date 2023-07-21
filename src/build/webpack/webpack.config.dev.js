726b3c1a5963ff062f2658ecf993fb155df0c313
vebnor <vebjorn.nordby@nav.no>
2023-07-21 16:20:28 +0200

    msw

--- src/build/webpack/webpack.config.dev.js
+++ src/build/webpack/webpack.config.dev.js
@@ -35,7 +35,6 @@ webpackConfig.plugins.push(
     }),
 );
 
-webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
 webpackConfig.plugins.push(new ReactRefreshWebpackPlugin());
 webpackConfig.plugins.push(new webpack.EnvironmentPlugin({ MSW_MODE: 'development' }));
 
