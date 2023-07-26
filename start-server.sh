2e0a0ce7bd6ba550062610de05c25e7e3458333a
vebnor <vebjorn.nordby@nav.no>
2023-07-26 08:31:13 +0200

    prøver igjen

--- start-server.sh
+++ start-server.sh
@@ -8,6 +8,7 @@ envsubst < /usr/share/nginx/html/dist/envVariablesForEnvSubst.json > /tmp/k9-pun
 export APP_HOSTNAME="${HOSTNAME:-localhost}"
 export APP_PORT="${APP_PORT:-443}"
 export APP_NAME="${APP_NAME:-devimg}"
+export OIDC_AUTH_PROXY="${OIDC_AUTH_PROXY:-http://localhost:8080}"
 
 envsubst '$APP_PORT $APP_HOSTNAME $APP_NAME $OIDC_AUTH_PROXY' < /etc/nginx/conf.d/app.conf.template > /etc/nginx/conf.d/default.conf
 
