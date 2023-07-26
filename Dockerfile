7c7a0088a28710bc70ce9120f0e70f5a085fbf6f
vebnor <vebjorn.nordby@nav.no>
2023-07-26 15:51:30 +0200

    går for nginxinc image i stedet for chainguard da det er problematisk å sette miljøvariabler i chainguard imaget

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,9 @@
-# Stage 1: Perform environment substitution in a full-featured base image
-FROM alpine:3.14 AS builder
+FROM nginxinc/nginx-unprivileged:1.25
 
-RUN apk add --no-cache gettext
 COPY dist /usr/share/nginx/html/dist
 COPY dist/index.html /usr/share/nginx/html/index.html
 COPY server.nginx /etc/nginx/conf.d/app.conf.template
 COPY start-server.sh /start-server.sh
 
-RUN sh /start-server.sh
+CMD sh /start-server.sh
 
-# Stage 2: Copy the resulting files into a distroless base image
-FROM cgr.dev/chainguard/nginx:latest
-COPY --from=builder /usr/share/nginx/html/dist /usr/share/nginx/html/dist
-COPY --from=builder /tmp/k9-punsj/env.json /usr/share/nginx/html/getEnvVariables
-COPY --from=builder /usr/share/nginx/html /usr/share/nginx/html
-COPY --from=builder /etc/nginx/conf.d /etc/nginx/conf.d
--- start-server.sh
+++ start-server.sh
@@ -1,23 +1,26 @@
 #!/usr/bin/env bash
 set -e
 
-
+_shutdown_() {
+  # https://nav-it.slack.com/archives/C5KUST8N6/p1543497847341300
+  echo "shutdown initialized, allowing incoming requests for 5 seconds before continuing"
+  sleep 5
+  nginx -s quit
+  wait "$pid"
+}
 
 [ -d /tmp/k9-punsj/] && echo "Feature toggle-directory finnes fra før, tilbakestiller" && rm -r /tmp/k9-punsj/* || mkdir -p  /tmp/k9-punsj/
-# echo the  OIDC_AUTH_PROXY env variable
-echo "OIDC_AUTH_PROXY: $OIDC_AUTH_PROXY"
-printenv
-
 envsubst < /usr/share/nginx/html/dist/envVariablesForEnvSubst.json > /tmp/k9-punsj/env.json
 export APP_HOSTNAME="${HOSTNAME:-localhost}"
 export APP_PORT="${APP_PORT:-443}"
 export APP_NAME="${APP_NAME:-devimg}"
-export OIDC_AUTH_PROXY="${OIDC_AUTH_PROXY:-http://localhost:8080}"
-echo "OIDC_AUTH_PROXY: $OIDC_AUTH_PROXY"
-# print all env variables
-printenv
+
 envsubst '$APP_PORT $APP_HOSTNAME $APP_NAME $OIDC_AUTH_PROXY' < /etc/nginx/conf.d/app.conf.template > /etc/nginx/conf.d/default.conf
 
 
 echo "### Nginx conf ###"
 cat /etc/nginx/conf.d/default.conf
+
+nginx -g "daemon off;" &
+pid=$!
+wait "$pid"
