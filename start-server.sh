80923024800001ad42dcf8c231f97d4c59f1cf16
vebnor <vebjorn.nordby@nav.no>
2023-07-26 08:45:25 +0200

    debugging

--- start-server.sh
+++ start-server.sh
@@ -4,12 +4,17 @@ set -e
 
 
 [ -d /tmp/k9-punsj/] && echo "Feature toggle-directory finnes fra før, tilbakestiller" && rm -r /tmp/k9-punsj/* || mkdir -p  /tmp/k9-punsj/
+# echo the  OIDC_AUTH_PROXY env variable
+echo "OIDC_AUTH_PROXY: $OIDC_AUTH_PROXY"
+
 envsubst < /usr/share/nginx/html/dist/envVariablesForEnvSubst.json > /tmp/k9-punsj/env.json
 export APP_HOSTNAME="${HOSTNAME:-localhost}"
 export APP_PORT="${APP_PORT:-443}"
 export APP_NAME="${APP_NAME:-devimg}"
 export OIDC_AUTH_PROXY="${OIDC_AUTH_PROXY:-http://localhost:8080}"
-
+echo "OIDC_AUTH_PROXY: $OIDC_AUTH_PROXY"
+# print all env variables
+printenv
 envsubst '$APP_PORT $APP_HOSTNAME $APP_NAME $OIDC_AUTH_PROXY' < /etc/nginx/conf.d/app.conf.template > /etc/nginx/conf.d/default.conf
 
 
