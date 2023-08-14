281d6d718ce1a407d7606194e31e244f75bb7adc
vebnor <vebjorn.nordby@nav.no>
2023-08-14 12:41:53 +0200

    bump

--- src/test/testUtils.tsx
+++ src/test/testUtils.tsx
@@ -1,5 +1,4 @@
 import { render } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
 import React, { ReactNode } from 'react';
 import { IntlShape } from 'react-intl';
 import { Provider } from 'react-redux';
@@ -18,13 +17,6 @@ export const testIntl: IntlShape = {
 };
 
 export const renderWithIntl = (component: ReactNode) => render(<IntlProvider locale="nb">{component}</IntlProvider>);
-
-export const renderWithRouterAndIntl = (component: ReactNode, history: any = createMemoryHistory({})) =>
-    render(
-        <Router history={history}>
-            <IntlProvider locale="nb">{component}</IntlProvider>
-        </Router>,
-    );
 
 interface IReduxTestProvider {
     initialState?: RootStateType;
