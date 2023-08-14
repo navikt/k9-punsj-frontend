76d1d36c94e89ce1288b52f08be69fb0001afe98
vebnor <vebjorn.nordby@nav.no>
2023-08-14 16:17:06 +0200

    bugfix

--- src/app/components/pdf/PdfVisning.tsx
+++ src/app/components/pdf/PdfVisning.tsx
@@ -106,7 +106,7 @@ const PdfVisning: React.FunctionComponent<IPdfVisningProps> = ({ journalpostDoku
                             value={aktivtDokument}
                         >
                             {dokumenter.map((_: unknown, i: number, array: unknown[]) => (
-                                <ToggleGroup.Item key="i" value={String(i + 1)}>
+                                <ToggleGroup.Item key={i} value={String(i + 1)}>
                                     {`Dokument ${i + 1} / ${array.length}`}
                                 </ToggleGroup.Item>
                             ))}
