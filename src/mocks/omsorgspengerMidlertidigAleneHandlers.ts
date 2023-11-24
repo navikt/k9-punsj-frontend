60be1cb78e4bb78dcfed109f9c010ba2c13258b2
Aleksei Poroshin <aleksei.poroshin@nav.no>
2023-11-24 14:58:26 +0100

    fix msw major upd

--- src/mocks/omsorgspengerMidlertidigAleneHandlers.ts
+++ src/mocks/omsorgspengerMidlertidigAleneHandlers.ts
@@ -1,64 +1,86 @@
-import { rest } from 'msw';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { HttpResponse, http } from 'msw';
 
 import { LOCAL_API_URL } from './konstanter';
 
 const handlers = {
-    tomMappe: rest.get(`${LOCAL_API_URL}/omsorgspenger-midlertidig-alene-soknad/mappe`, (req, res, ctx) =>
-        res(
-            ctx.json({
-                søker: '29099000129',
-                fagsakTypeKode: 'OMP_MA',
-                søknader: [],
-            }),
-        ),
+    tomMappe: http.get(`${LOCAL_API_URL}/omsorgspenger-midlertidig-alene-soknad/mappe`, () =>
+        HttpResponse.json({
+            søker: '29099000129',
+            fagsakTypeKode: 'OMP_MA',
+            søknader: [],
+        }),
     ),
-    mappeMedSøknad: rest.get(`${LOCAL_API_URL}/omsorgspenger-midlertidig-alene-soknad/mappe`, (req, res, ctx) =>
-        res(
-            ctx.json({
-                søker: '29099000129',
-                fagsakTypeKode: 'OMP_MA',
-                søknader: [
-                    {
-                        soeknadId: 'db054295-f1bd-45d2-b0fe-0d032ce25295',
-                        soekerId: '29099000129',
-                        mottattDato: '2020-10-12',
-                        klokkeslett: '12:53',
-                        barn: [
-                            {
-                                norskIdent: '02021477330',
-                                foedselsdato: null,
-                            },
-                            {
-                                norskIdent: '03091477490',
-                                foedselsdato: null,
-                            },
-                            {
-                                norskIdent: '09081478047',
-                                foedselsdato: null,
-                            },
-                        ],
-                        annenForelder: {
-                            norskIdent: '13079438906',
-                            situasjonType: 'UTØVER_VERNEPLIKT',
-                            situasjonBeskrivelse: 'asdasdasgfdfdfdf',
-                            periode: {
-                                fom: '2023-05-03',
-                                tom: '2023-05-18',
-                            },
+
+    mappeMedSøknad: http.get(`${LOCAL_API_URL}/omsorgspenger-midlertidig-alene-soknad/mappe`, () =>
+        HttpResponse.json({
+            søker: '29099000129',
+            fagsakTypeKode: 'OMP_MA',
+            søknader: [
+                {
+                    soeknadId: 'db054295-f1bd-45d2-b0fe-0d032ce25295',
+                    soekerId: '29099000129',
+                    mottattDato: '2020-10-12',
+                    klokkeslett: '12:53',
+                    barn: [
+                        {
+                            norskIdent: '02021477330',
+                            foedselsdato: null,
                         },
-                        journalposter: ['205'],
-                        harInfoSomIkkeKanPunsjes: false,
-                        harMedisinskeOpplysninger: false,
-                        metadata: null,
+                        {
+                            norskIdent: '03091477490',
+                            foedselsdato: null,
+                        },
+                        {
+                            norskIdent: '09081478047',
+                            foedselsdato: null,
+                        },
+                    ],
+                    annenForelder: {
+                        norskIdent: '13079438906',
+                        situasjonType: 'UTØVER_VERNEPLIKT',
+                        situasjonBeskrivelse: 'asdasdasgfdfdfdf',
+                        periode: {
+                            fom: '2023-05-03',
+                            tom: '2023-05-18',
+                        },
                     },
-                ],
-            }),
+                    journalposter: ['205'],
+                    harInfoSomIkkeKanPunsjes: false,
+                    harMedisinskeOpplysninger: false,
+                    metadata: null,
+                },
+            ],
+        }),
+    ),
+
+    nySoeknad: http.post(`${LOCAL_API_URL}/omsorgspenger-midlertidig-alene-soknad`, () =>
+        HttpResponse.json(
+            {
+                soeknadId: 'db054295-f1bd-45d2-b0fe-0d032ce25295',
+                soekerId: '29099000129',
+                mottattDato: '2020-10-12',
+                klokkeslett: '12:53',
+                barn: [],
+                annenForelder: {
+                    norskIdent: '07827599802',
+                    situasjonType: null,
+                    situasjonBeskrivelse: null,
+                    periode: null,
+                },
+                journalposter: ['205'],
+                harInfoSomIkkeKanPunsjes: null,
+                harMedisinskeOpplysninger: null,
+                metadata: null,
+            },
+            { status: 201 },
         ),
     ),
-    nySoeknad: rest.post(`${LOCAL_API_URL}/omsorgspenger-midlertidig-alene-soknad`, (req, res, ctx) =>
-        res(
-            ctx.status(201),
-            ctx.json({
+
+    soknad: http.get(
+        `${LOCAL_API_URL}/omsorgspenger-midlertidig-alene-soknad/mappe/db054295-f1bd-45d2-b0fe-0d032ce25295`,
+        () =>
+            HttpResponse.json({
                 soeknadId: 'db054295-f1bd-45d2-b0fe-0d032ce25295',
                 soekerId: '29099000129',
                 mottattDato: '2020-10-12',
@@ -75,38 +97,12 @@ const handlers = {
                 harMedisinskeOpplysninger: null,
                 metadata: null,
             }),
-        ),
     ),
-    soknad: rest.get(
-        `${LOCAL_API_URL}/omsorgspenger-midlertidig-alene-soknad/mappe/db054295-f1bd-45d2-b0fe-0d032ce25295`,
-        (req, res, ctx) =>
-            res(
-                ctx.json({
-                    soeknadId: 'db054295-f1bd-45d2-b0fe-0d032ce25295',
-                    soekerId: '29099000129',
-                    mottattDato: '2020-10-12',
-                    klokkeslett: '12:53',
-                    barn: [],
-                    annenForelder: {
-                        norskIdent: '07827599802',
-                        situasjonType: null,
-                        situasjonBeskrivelse: null,
-                        periode: null,
-                    },
-                    journalposter: ['205'],
-                    harInfoSomIkkeKanPunsjes: null,
-                    harMedisinskeOpplysninger: null,
-                    metadata: null,
-                }),
-            ),
-    ),
-    oppdater: rest.put(`${LOCAL_API_URL}/omsorgspenger-midlertidig-alene-soknad/oppdater`, (req, res, ctx) =>
-        res(ctx.json({})),
-    ),
-    sendInn: rest.post(`${LOCAL_API_URL}/omsorgspenger-midlertidig-alene-soknad/send`, (req, res, ctx) =>
-        res(
-            ctx.status(202),
-            ctx.json({
+
+    oppdater: http.put(`${LOCAL_API_URL}/omsorgspenger-midlertidig-alene-soknad/oppdater`, () => HttpResponse.json({})),
+    sendInn: http.post(`${LOCAL_API_URL}/omsorgspenger-midlertidig-alene-soknad/send`, () =>
+        HttpResponse.json(
+            {
                 søknadId: '78415bd0-dd83-471d-b9e1-018b57cfdc10',
                 versjon: '1.0.0',
                 mottattDato: '2020-10-12T10:53:00.000Z',
@@ -150,13 +146,14 @@ const handlers = {
                     tekst: null,
                 },
                 kildesystem: null,
-            }),
+            },
+            { status: 202 },
         ),
     ),
-    valider: rest.post(`${LOCAL_API_URL}/omsorgspenger-midlertidig-alene-soknad/valider`, (req, res, ctx) =>
-        res(
-            ctx.status(202),
-            ctx.json({
+
+    valider: http.post(`${LOCAL_API_URL}/omsorgspenger-midlertidig-alene-soknad/valider`, () =>
+        HttpResponse.json(
+            {
                 søknadId: '78415bd0-dd83-471d-b9e1-018b57cfdc10',
                 versjon: '1.0.0',
                 mottattDato: '2020-10-12T10:53:00.000Z',
@@ -200,13 +197,14 @@ const handlers = {
                     tekst: null,
                 },
                 kildesystem: null,
-            }),
+            },
+            { status: 202 },
         ),
     ),
-    validerFeil: rest.post(`${LOCAL_API_URL}/omsorgspenger-midlertidig-alene-soknad/valider`, (req, res, ctx) =>
-        res(
-            ctx.status(400),
-            ctx.json({
+
+    validerFeil: http.post(`${LOCAL_API_URL}/omsorgspenger-midlertidig-alene-soknad/valider`, () =>
+        HttpResponse.json(
+            {
                 feil: [
                     {
                         felt: 'feil',
@@ -215,7 +213,8 @@ const handlers = {
                     },
                 ],
                 søknadIdDto: '27b0ffe6-26b3-4381-94f4-574ce4022b08',
-            }),
+            },
+            { status: 400 },
         ),
     ),
 };
