1c89c7b0c4e003cc6061911410e1233490ae3568
vebnor <vebjorn.nordby@nav.no>
2023-12-12 15:32:32 +0100

    tester for dokumentvalg etter bug

--- src/test/components/pdf/PdfVisning.spec.tsx
+++ src/test/components/pdf/PdfVisning.spec.tsx
@@ -1,6 +1,5 @@
-import { expect } from '@jest/globals';
-import { screen } from '@testing-library/react';
 import React from 'react';
+import { screen, waitFor } from '@testing-library/react';
 
 import PdfVisning from '../../../app/components/pdf/PdfVisning';
 import { renderWithIntl } from '../../testUtils';
@@ -18,5 +17,66 @@ describe('<PdfVisning>', () => {
         renderWithIntl(<PdfVisning journalpostDokumenter={journalpostDokumenter} />);
 
         expect(screen.getByTitle('pdf')).toBeDefined();
+    });
+
+    it('riktig pdf-url på dokument', () => {
+        window.history.pushState({}, 'Test Title', '/test');
+        const journalpostid = '200';
+        const dokumentid = { dokumentId: '123' };
+        const journalpostDokumenter = [{ journalpostid, dokumenter: [dokumentid] }];
+        renderWithIntl(<PdfVisning journalpostDokumenter={journalpostDokumenter} />);
+
+        expect(screen.getByTitle('pdf')).toHaveAttribute(
+            'src',
+            'http://localhost:8101/api/k9-punsj/journalpost/200/dokument/123',
+        );
+    });
+
+    it('iframe viser dokument 2 hvis ?dok=2 er i url', () => {
+        window.history.pushState({}, 'Test Title', '/test?dok=2');
+        const journalpostid = '200';
+        const dokumenter = [{ dokumentId: '123' }, { dokumentId: '456' }];
+
+        const journalpostDokumenter = [{ journalpostid, dokumenter }];
+        renderWithIntl(<PdfVisning journalpostDokumenter={journalpostDokumenter} />);
+
+        expect(screen.getByTitle('pdf')).toHaveAttribute(
+            'src',
+            'http://localhost:8101/api/k9-punsj/journalpost/200/dokument/456',
+        );
+    });
+
+    it('Skal vise toggle knapper likt antall dokumenter i journalpost', async () => {
+        window.history.pushState({}, 'Test Title', '/test?dok=1');
+
+        const journalpostid = '200';
+        const dokumenter = [{ dokumentId: '123' }, { dokumentId: '456' }];
+        const journalpostDokumenter = [{ journalpostid, dokumenter }];
+        renderWithIntl(<PdfVisning journalpostDokumenter={journalpostDokumenter} />);
+
+        expect(screen.getByText('Dokument 1 / 2')).toBeDefined();
+        expect(screen.getByText('Dokument 2 / 2')).toBeDefined();
+
+        expect(screen.getByTitle('pdf')).toHaveAttribute(
+            'src',
+            'http://localhost:8101/api/k9-punsj/journalpost/200/dokument/123',
+        );
+
+        screen.getByTestId('dok-2').click();
+
+        await waitFor(() => {
+            expect(screen.getByTitle('pdf')).toHaveAttribute(
+                'src',
+                'http://localhost:8101/api/k9-punsj/journalpost/200/dokument/456',
+            );
+        });
+
+        screen.getByTestId('dok-1').click();
+        await waitFor(() => {
+            expect(screen.getByTitle('pdf')).toHaveAttribute(
+                'src',
+                'http://localhost:8101/api/k9-punsj/journalpost/200/dokument/123',
+            );
+        });
     });
 });
