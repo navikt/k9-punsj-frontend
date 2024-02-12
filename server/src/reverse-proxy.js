8d64517eab24d78cd2a73f8ebfe050fcb7c918c5
vebnor <vebjorn.nordby@nav.no>
2024-02-12 18:32:49 +0100

    /k9-punsj i url er ikke en greie i backend. Fjerner det

--- server/src/reverse-proxy.js
+++ server/src/reverse-proxy.js
@@ -30,15 +30,13 @@ const proxyOptions = (api) => ({
         );
     },
     proxyReqPathResolver: (req) => {
-        console.log(req.headers.authorization);
         const urlFromApi = url.parse(api.url);
-        console.log('url from api', urlFromApi);
         const pathFromApi = urlFromApi.pathname === '/' ? '' : urlFromApi.pathname;
-        console.log('path from api', pathFromApi);
         const urlFromRequest = url.parse(req.originalUrl);
-        console.log('url from request', urlFromRequest);
-        const pathFromRequest = urlFromRequest.pathname;
-        console.log('path from request', pathFromRequest);
+        let pathFromRequest = urlFromRequest.pathname;
+
+        // Remove /k9-punsj from the path
+        pathFromRequest = pathFromRequest.replace('/k9-punsj', '');
         const queryString = urlFromRequest.query;
         const newPath = (pathFromApi || '') + (pathFromRequest || '') + (queryString ? `?${queryString}` : '');
 
