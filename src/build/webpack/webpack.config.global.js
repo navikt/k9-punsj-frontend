71d89d33508a083e7de3b5c5594b169a0abd1158
vebnor <vebjorn.nordby@nav.no>
2024-03-12 19:26:04 +0100

    prøver å bruke nais.js runtime

--- src/build/webpack/webpack.config.global.js
+++ src/build/webpack/webpack.config.global.js
@@ -76,7 +76,7 @@ const webpackConfig = {
         new webpack.EnvironmentPlugin({ SENTRY_RELEASE: null }),
     ],
     externals: {
-        '../build/webpack/faroConfig'
+        '../build/webpack/faroConfig': 'excludedFile'
     },
 };
 
