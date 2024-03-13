4db4e12b8a6ef4d5296ee6de8ed61f3eb09322c9
vebnor <vebjorn.nordby@nav.no>
2024-03-13 14:09:30 +0100

    init faro

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,7 +13,6 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { applyMiddleware, legacy_createStore } from 'redux';
 import logger from 'redux-logger';
 import { initializeFaro } from '@grafana/faro-web-sdk';
-import faroConfig from '../build/webpack/faroConfig';
 
 import '@navikt/ds-css';
 import '@navikt/ft-plattform-komponenter/dist/style.css';
