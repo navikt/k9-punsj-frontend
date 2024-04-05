7d9ab02ba74fc78f939381cb3b31cc280ea693a7
vebnor <vebjorn.nordby@nav.no>
2024-04-05 14:21:46 +0200

    legger til logging av error-statuser på alle funksjoner for api-kall

--- src/app/components/brev/PreviewMessage.ts
+++ src/app/components/brev/PreviewMessage.ts
@@ -81,6 +81,7 @@ export const previewMessage = async (
 
         throw new Error(response.statusText);
     } catch (error) {
+        console.error('Error i forhåndsvisning av brev', error);
         return error.message as string;
     }
 };
