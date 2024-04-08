adc06165e7deb89232d2b0607ba0f429b7b50981
Vebjørn Nordby <vebjorn.nordby@nav.no>
2024-04-08 12:25:58 +0200

    Logging grafana faro (#2268)
    * debugge logging
    
    * faro init

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -41,11 +41,16 @@ Sentry.init({
     beforeSend: (event) => event,
 });
 
-function prepare() {
+async function prepare() {
+    initializeFaro({
+        url: window.nais?.telemetryCollectorURL,
+        app: window.nais?.app,
+        instrumentations: [...getWebInstrumentations({ captureConsole: true })],
+    });
     if (process.env.NODE_ENV !== 'production') {
         return import('../mocks/browser').then(({ worker }) => worker.start({ onUnhandledRequest: 'bypass' }));
+        
     }
-
     return Promise.resolve();
 }
 
@@ -68,14 +73,6 @@ queryClient.setDefaultOptions({
 // eslint-disable-next-line import/prefer-default-export
 export const App: React.FunctionComponent = () => {
     const [locale, setLocale] = React.useState<Locale>(localeFromSessionStorage);
-
-    React.useEffect(() => {
-        initializeFaro({
-            url: window.nais?.telemetryCollectorURL,
-            app: window.nais?.app,
-            instrumentations: [...getWebInstrumentations({ captureConsole: true })],
-        });
-    }, [window.nais?.telemetryCollectorURL, window.nais?.app]);
 
     return (
         <Sentry.ErrorBoundary>
