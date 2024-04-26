973182c2134b944d9482949f628bd8181de26c12
Aleksei Poroshin <aleksei.poroshin@nav.no>
2024-04-26 12:14:39 +0200

    fix

--- src/app/containers/pleiepenger/Fordeling/Komponenter/KlassifiserModal.tsx
+++ src/app/containers/pleiepenger/Fordeling/Komponenter/KlassifiserModal.tsx
@@ -186,12 +186,14 @@ const KlassifiserModal = ({ lukkModal, setFagsak, dedupkey, fortsett, behandling
             journalførJournalpost.isSuccess && (journalførJournalpost.data.saksnummer as string)
                 ? (journalførJournalpost.data.saksnummer as string)
                 : '';
+        const reservertFagsakIdEtterKopier =
+            getJournalpost.isSuccess && getJournalpost.data.sak?.fagsakId ? getJournalpost.data.sak?.fagsakId : '';
         const threeWeeksDate = get3WeeksDate();
         const messageContent = message || (
             <FormattedMessage
                 id={messageId}
                 values={{
-                    saksnummer: reservertFagsakId,
+                    saksnummer: reservertFagsakId || reservertFagsakIdEtterKopier,
                     dato: threeWeeksDate,
                     annenSøkerFnr: identState.annenSokerIdent,
                     pleietrengendeFnr: identState.pleietrengendeId,
