0bf19bdf342a6782954b1bbcf772c3871427330a
Aleksei Poroshin <aleksei.poroshin@nav.no>
2024-11-14 17:21:12 +0100

    fix

--- src/app/søknader/korrigeringAvInntektsmelding/containers/BekreftInnsendingModal/BekreftInnsendingModal.tsx
+++ src/app/søknader/korrigeringAvInntektsmelding/containers/BekreftInnsendingModal/BekreftInnsendingModal.tsx
@@ -1,37 +1,39 @@
 import React from 'react';
-import { useIntl } from 'react-intl';
 
+import { FormattedMessage } from 'react-intl';
+import classNames from 'classnames';
 import { Button } from '@navikt/ds-react';
-
-import intlHelper from 'app/utils/intlUtils';
 
 import { KorrigeringAvInntektsmeldingFormValues } from '../../types/KorrigeringAvInntektsmeldingFormFieldsValues';
 import OMSKvittering from '../SøknadKvittering/OMSKvittering';
+
 import './bekreftInnsendingModal.less';
 
-interface BekreftInnsendingModalProps {
+interface Props {
     feltverdier: KorrigeringAvInntektsmeldingFormValues;
     lukkModal: () => void;
     handleVidere: () => void;
 }
 
-const BekreftInnsendingModal: React.FC<BekreftInnsendingModalProps> = ({ feltverdier, lukkModal, handleVidere }) => {
-    const intl = useIntl();
-
+const BekreftInnsendingModal: React.FC<Props> = ({ feltverdier, lukkModal, handleVidere }) => {
     return (
         <div className="bekreftInnsendingModal">
-            <OMSKvittering feltverdier={feltverdier} />
+            <div className={classNames('validertSoknadOppsummeringContainer')}>
+                <OMSKvittering feltverdier={feltverdier} />
+            </div>
+
             <div className="bekreftInnsendingModal__buttonContainer">
                 <Button size="small" className="bekreftInnsendingModal__knappVidere" onClick={() => handleVidere()}>
-                    {intlHelper(intl, 'fordeling.knapp.videre')}
+                    <FormattedMessage id="fordeling.knapp.videre" />
                 </Button>
+
                 <Button
                     variant="secondary"
                     size="small"
                     className="validertSoknadOppsummeringContainer_knappTilbake"
                     onClick={() => lukkModal()}
                 >
-                    {intlHelper(intl, 'skjema.knapp.avbryt')}
+                    <FormattedMessage id="skjema.knapp.avbryt" />
                 </Button>
             </div>
         </div>
