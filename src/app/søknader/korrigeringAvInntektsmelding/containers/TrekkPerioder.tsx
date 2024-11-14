70bdb00c4cf4d55d2fec10118a8eba347995e4d8
Aleksei Poroshin <aleksei.poroshin@nav.no>
2024-11-14 15:58:20 +0100

    fix

--- src/app/søknader/korrigeringAvInntektsmelding/containers/TrekkPerioder.tsx
+++ src/app/søknader/korrigeringAvInntektsmelding/containers/TrekkPerioder.tsx
@@ -1,40 +1,41 @@
 import React from 'react';
-import { useIntl } from 'react-intl';
 
-import { Alert, Fieldset, Panel } from '@navikt/ds-react';
+import { FormattedMessage, useIntl } from 'react-intl';
+import { Alert, Box, Fieldset } from '@navikt/ds-react';
 
 import PanelProps from 'app/models/types/korrigeringAvInntektsmelding/Paneler';
 import intlHelper from 'app/utils/intlUtils';
-
 import EkspanderbartPanel from '../../../components/EkspanderbartPanel';
 import { KorrigeringAvInntektsmeldingFormFields } from '../types/KorrigeringAvInntektsmeldingFormFieldsValues';
 import { Periodepanel } from '../components/Periodepanel';
 
 const TrekkPerioder: React.FC<PanelProps> = ({ isPanelOpen, togglePanel }): JSX.Element => {
     const intl = useIntl();
+
     return (
         <EkspanderbartPanel
             label={intlHelper(intl, 'omsorgspenger.korrigeringAvInntektsmelding.trekkPeriode.checkbox')}
             isPanelOpen={isPanelOpen}
             togglePanel={togglePanel}
         >
-            <Panel className="listepanel">
+            <Box padding="4" borderWidth="1" borderRadius="small" className="listepanel">
                 <Fieldset
                     legend={
                         <h4 className="korrigering-legend">
-                            {intlHelper(intl, 'omsorgspenger.korrigeringAvInntektsmelding.trekkPeriode.legend')}
+                            <FormattedMessage id="omsorgspenger.korrigeringAvInntektsmelding.trekkPeriode.legend" />
                         </h4>
                     }
                     className="korrigering__Fieldset"
                 >
                     <Alert size="small" variant="info" className="korrigering__infostripe">
-                        {intlHelper(intl, 'omsorgspenger.korrigeringAvInntektsmelding.trekkPeriode.info')}
+                        <FormattedMessage id="omsorgspenger.korrigeringAvInntektsmelding.trekkPeriode.info" />
                     </Alert>
+
                     <div className="soknadsperiodecontainer">
                         <Periodepanel name={KorrigeringAvInntektsmeldingFormFields.Trekkperioder} />
                     </div>
                 </Fieldset>
-            </Panel>
+            </Box>
         </EkspanderbartPanel>
     );
 };
