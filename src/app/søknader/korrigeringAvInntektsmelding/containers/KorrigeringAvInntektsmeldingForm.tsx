a5029f2b2ed0ae31edac290500c8019dcbcde240
Aleksei Poroshin <aleksei.poroshin@nav.no>
2024-11-14 22:56:41 +0100

    fix

--- src/app/søknader/korrigeringAvInntektsmelding/containers/KorrigeringAvInntektsmeldingForm.tsx
+++ src/app/søknader/korrigeringAvInntektsmelding/containers/KorrigeringAvInntektsmeldingForm.tsx
@@ -127,10 +127,6 @@ const KorrigeringAvInntektsmeldingForm: React.FC<Props> = ({ søkerId, søknadId
         );
     }
 
-    // eslint-disable-next-line no-console
-    console.log('TEST formError:', formError);
-    // eslint-disable-next-line no-console
-    console.log('TEST HasSubmittet:', hasSubmitted);
     return (
         <Formik
             initialValues={{
