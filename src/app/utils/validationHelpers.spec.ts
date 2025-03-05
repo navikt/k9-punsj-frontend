cb924c0c6da3b1438bdeeb6d132e2f225802c9cf
Hallvard Andreas Stark <hallvard@scel.to>
2025-03-05 09:28:15 +0100

    En test til

--- src/app/utils/validationHelpers.spec.ts
+++ src/app/utils/validationHelpers.spec.ts
@@ -5,21 +5,28 @@ describe('validationHelpers', () => {
         it('returns true for person younger than 18', () => {
             const today = new Date();
             const year = today.getFullYear();
-            const youngPerson = `0101${(year - 15).toString().slice(-2)}999`;
+            const youngPerson = `0101${(year - 15).toString().slice(-2)}99900`;
             expect(erYngreEnn18år(youngPerson)).toBe(true);
         });
 
         it('returns false for person older than 18', () => {
             const today = new Date();
             const year = today.getFullYear();
-            const adultPerson = `0101${(year - 20).toString().slice(-2)}999`;
+            const adultPerson = `0101${(year - 20).toString().slice(-2)}99900`;
+            expect(erYngreEnn18år(adultPerson)).toBe(false);
+        });
+
+        it('returns false for person born before 2000', () => {
+            const today = new Date();
+            const year = today.getFullYear();
+            const adultPerson = `0101${(year - 50).toString().slice(-2)}25000`;
             expect(erYngreEnn18år(adultPerson)).toBe(false);
         });
 
         it('returns false for an exact 18-year-old', () => {
             const today = new Date();
             const year = today.getFullYear();
-            const exactEighteenPerson = `0101${(year - 18).toString().slice(-2)}999`;
+            const exactEighteenPerson = `0101${(year - 18).toString().slice(-2)}99900`;
             expect(erYngreEnn18år(exactEighteenPerson)).toBe(false);
         });
     });
