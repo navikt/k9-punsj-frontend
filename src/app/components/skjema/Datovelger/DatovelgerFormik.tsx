6ed00fb3dc0ed7cb95a761c1c4c59cce54c666c4
vebnor <vebjorn.nordby@nav.no>
2025-10-16 08:03:01 +0200

    viser border bare når feltet er touched

--- src/app/components/skjema/Datovelger/DatovelgerFormik.tsx
+++ src/app/components/skjema/Datovelger/DatovelgerFormik.tsx
@@ -22,6 +22,16 @@ const DatovelgerFormik = ({
     visFeilmelding = true,
 }: Props) => {
     const [field, meta, helper] = useField(name);
+
+    const getErrorMessage = () => {
+        // Vi viser feilmelding hvis visFeilmelding er true og feltet har blitt besøkt og har en feilmelding
+        if (visFeilmelding) {
+            return meta.touched && meta.error ? meta.error : undefined;
+        }
+        // Hvis vi ikke skal vise feilmelding, så må vi uansett sende inn boolean når vi har en feil
+        // for å få rød border på inputfeltet
+        return meta.touched && !!meta.error;
+    };
     return (
         <Datovelger
             label={label}
@@ -29,7 +39,7 @@ const DatovelgerFormik = ({
             selectedDay={field.value}
             onChange={(value) => helper.setValue(value)}
             onBlur={() => helper.setTouched(true, true)}
-            errorMessage={meta.touched && visFeilmelding ? meta.error : !!meta.error}
+            errorMessage={getErrorMessage()}
             toDate={toDate}
             fromDate={fromDate}
             hideLabel={hideLabel}
