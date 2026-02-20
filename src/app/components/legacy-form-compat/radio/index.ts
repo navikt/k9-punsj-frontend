00a3b94abff5c5fc3bb967b18d877661c18b3dcf
Aleksei Poroshin <aleksei.poroshin@nav.no>
2026-02-20 21:38:49 +0100

    refactor: migrer fordeling radioer til legacy compat

--- src/app/components/legacy-form-compat/radio/index.ts
+++ src/app/components/legacy-form-compat/radio/index.ts
@@ -2,3 +2,5 @@ export { default as LegacyRadio } from './Radio';
 export type { LegacyRadioProps } from './Radio';
 export { default as LegacyRadioGroup } from './RadioGroup';
 export type { LegacyRadioGroupOption, LegacyRadioGroupProps } from './RadioGroup';
+export { default as LegacyJaNeiRadioGroup } from './JaNeiRadioGroup';
+export type { LegacyJaNeiRadioGroupProps } from './JaNeiRadioGroup';
