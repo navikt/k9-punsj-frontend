63e99122b9aa838b6827f4b9b3659a129a6a41f7
Aleksei Poroshin <aleksei.poroshin@nav.no>
2026-02-23 12:37:48 +0100

    fix: legg til lokal mock-pdf for devserver i testmodus

--- src/build/webpack/devserver.config.mjs
+++ src/build/webpack/devserver.config.mjs
@@ -3,6 +3,7 @@ import mustacheExpress from 'mustache-express';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import { envVariables } from '@k9-punsj-frontend/server/envVariables.js';
+import { createMockPdfBytes } from '../../mocks/mockPdf.js';
 
 dotenv.config();
 
@@ -26,6 +27,8 @@ const configureDevServer = () => ({
     proxy: proxyConfig,
     setupMiddlewares: (middlewares, devServer) => {
         const { app } = devServer;
+        const mockPdfBytes = createMockPdfBytes();
+
         app.engine('html', mustacheExpress());
         app.set('views', path.resolve(__dirname, '../../../dist'));
         app.set('view engine', 'mustache');
@@ -56,6 +59,15 @@ const configureDevServer = () => ({
             res.set('service-worker-allowed', '/');
             res.sendFile(path.resolve(__dirname, '../../mocks/mockServiceWorker.js'));
         });
+
+        if (process.env.MSW_MODE === 'test') {
+            app.get(/^\/api\/k9-punsj\/journalpost\/[^/]+\/dokument\/[^/]+\/?$/, (req, res) => {
+                res.set('content-type', 'application/pdf');
+                res.set('content-disposition', 'inline; filename="mock-dokument.pdf"');
+                res.set('cache-control', 'no-store');
+                res.status(200).send(Buffer.from(mockPdfBytes));
+            });
+        }
 
         return middlewares;
     },
