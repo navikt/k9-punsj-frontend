4c4c66d7f5ba587bb94d7b52e0ea38233132793d
Aleksei Poroshin <aleksei.poroshin@nav.no>
2026-03-05 15:17:14 +0100

    pkgupd minor

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
     video: false,
+    allowCypressEnv: false,
     e2e: {
         baseUrl: 'http://localhost:8080',
         specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
