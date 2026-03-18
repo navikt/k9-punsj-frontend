c734075b0dc7b30713537dd538b300c227aadc9d
Aleksei Poroshin <aleksei.poroshin@nav.no>
2026-03-18 15:15:01 +0100

    test: tydeliggor testnavn for perioder

--- src/test/containers/pleiepenger/endringAvSoknadsperioderUtils.spec.ts
+++ src/test/containers/pleiepenger/endringAvSoknadsperioderUtils.spec.ts
@@ -2,7 +2,7 @@ import { IPeriode } from '../../../app/models/types/Periode';
 import { buildEndringAvSoknadsperioderUpdate } from '../../../app/søknader/pleiepenger/containers/EndringAvSøknadsperioder/EndringAvSøknadsperioder';
 
 describe('buildEndringAvSoknadsperioderUpdate', () => {
-    it('beholder kun trekkKravPerioder når listen ikke er tom', () => {
+    it('oppdaterer kun trekkKravPerioder når periodene overlapper eksisterende perioder', () => {
         const perioder: IPeriode[] = [{ fom: '2026-02-02', tom: '2026-02-20' }];
         const eksisterendePerioder: IPeriode[] = [{ fom: '2026-02-01', tom: '2026-02-28' }];
 
