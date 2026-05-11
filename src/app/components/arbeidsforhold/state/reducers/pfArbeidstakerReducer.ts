02a8026152fb19828b0460f9e811095c86fdac86
Aleksei Poroshin <aleksei.poroshin@nav.no>
2026-05-11 15:02:01 +0200

    refactor: fullfor use reducer typescript 6 batch

--- src/app/components/arbeidsforhold/state/reducers/pfArbeidstakerReducer.ts
+++ src/app/components/arbeidsforhold/state/reducers/pfArbeidstakerReducer.ts
@@ -15,10 +15,10 @@ interface Action {
     gjelderAnnenArbeidsgiver?: boolean;
 }
 
-const pfArbeidstakerReducer = (state: State, action: Action): Partial<State> => {
+const pfArbeidstakerReducer = (state: State, action: Action): State => {
     switch (action.type) {
         case ActionType.SELECT_ARBEIDSGIVER:
-            return { ...state, selectedArbeidsgiver: action.selectedArbeidsgiver };
+            return { ...state, selectedArbeidsgiver: action.selectedArbeidsgiver ?? state.selectedArbeidsgiver };
         case ActionType.TOGGLE_GJELDER_ANNEN_ARBEIDSGIVER:
             return {
                 ...state,
@@ -28,9 +28,17 @@ const pfArbeidstakerReducer = (state: State, action: Action): Partial<State> => 
                 searchOrganisasjonsnummerFailed: false,
             };
         case ActionType.SET_NAVN_ARBEIDSDGIVER:
-            return { ...state, navnPåArbeidsgiver: action.navnPåArbeidsgiver, searchOrganisasjonsnummerFailed: false };
+            return {
+                ...state,
+                navnPåArbeidsgiver: action.navnPåArbeidsgiver ?? state.navnPåArbeidsgiver,
+                searchOrganisasjonsnummerFailed: false,
+            };
         case ActionType.SET_SEARCH_ORGANISASJONSNUMMER_FAILED:
-            return { ...state, searchOrganisasjonsnummerFailed: action.searchOrganisasjonsnummerFailed };
+            return {
+                ...state,
+                searchOrganisasjonsnummerFailed:
+                    action.searchOrganisasjonsnummerFailed ?? state.searchOrganisasjonsnummerFailed,
+            };
         default:
             return state;
     }
