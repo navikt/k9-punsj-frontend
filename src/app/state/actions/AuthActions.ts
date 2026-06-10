b76eed25ffa41c9f34e3f20d4cd4a64936c66680
vebnor <vebjorn.nordby@nav.no>
2026-06-10 15:28:20 +0200

    nytt endeunkt for saksbehandlerinfo og tilganger

--- src/app/state/actions/AuthActions.ts
+++ src/app/state/actions/AuthActions.ts
@@ -13,6 +13,10 @@ interface IAuthRedirectAction {
 interface IAuthOkAction {
     type: AuthActionKeys.OK;
     name: string;
+    erSaksbehandler: boolean;
+    erVeileder: boolean;
+    harBasistilgang: boolean;
+    harHistoriskTilgang: boolean;
 }
 interface IAuthErrorAction {
     type: AuthActionKeys.ERROR;
@@ -27,8 +31,15 @@ export const checkAuth = () => async (dispatch: any) => {
         const response = await get(ApiPath.ME, { credentials: 'include' });
 
         if (response.status === 200) {
-            const { name } = await response.json();
-            dispatch({ type: AuthActionKeys.OK, name });
+            const { name, erSaksbehandler, erVeileder, harBasistilgang, harHistoriskTilgang } = await response.json();
+            dispatch({
+                type: AuthActionKeys.OK,
+                name,
+                erSaksbehandler: Boolean(erSaksbehandler),
+                erVeileder: Boolean(erVeileder),
+                harBasistilgang: Boolean(harBasistilgang),
+                harHistoriskTilgang: Boolean(harHistoriskTilgang),
+            });
         } else {
             const error = convertResponseToError(response);
             dispatch({ type: AuthActionKeys.ERROR, error });
