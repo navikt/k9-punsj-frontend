a00d29bb99ad8e693dc029f92fe9af47dfa6a1a0
Vebjørn Nordby <vebjorn.nordby@nav.no>
2026-06-11 12:18:09 +0200

    Reapply "Ta i bruk nytt endepunkt for tilgangssjekk og sperr historis… (#3706)
    …ke saker uten tilgang" (#3705)
    
    This reverts commit 43621f47762aa93922c863105a0c35d681fea4de.
    
    ### Behov / Bakgrunn
    
    
    ### Løsning
    
    
    ### Andre endringer

--- src/app/fordeling/Fordeling.tsx
+++ src/app/fordeling/Fordeling.tsx
@@ -93,6 +93,9 @@ const Fordeling: React.FC = () => {
     const identState = useSelector((state: RootStateType) => state.identState);
     const fellesState = useSelector((state: RootStateType) => state.felles);
     const opprettIGosysState = useSelector((state: RootStateType) => state.opprettIGosys);
+    const harHistoriskTilgang = useSelector(
+        (state: RootStateType) => state.authState.tilganger?.harHistoriskTilgang ?? false,
+    );
 
     const omfordel = (journalpostid: string, norskIdent: string, gosysKategori: string) =>
         dispatch(omfordelAction(journalpostid, norskIdent, gosysKategori));
@@ -717,6 +720,7 @@ const Fordeling: React.FC = () => {
                                     setBehandlingsAar={setBehandlingsAar}
                                     setAnnenPart={setAnnenPart}
                                     setFosterbarn={setFosterbarn}
+                                    harHistoriskTilgang={harHistoriskTilgang}
                                 />
                             )}
 
